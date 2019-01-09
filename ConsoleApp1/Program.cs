e6c5905d70b3814625bee25c981994c7fda3270d
Kapil Jain <kapil.jain@cleartax.in>
2019-01-09 14:02:56 +0530

    Adding indexes also

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,16 +16,29 @@ namespace ConsoleApp1
             List<string> output = new List<string>();
             Regex createTable = new Regex(@"[ ]*CREATE TABLE IF NOT EXISTS `[_a-zA-Z0-9]*`\.`([_a-zA-Z0-9]*)` \(");
             Regex contstraint = new Regex(@"[ ]*CONSTRAINT `[_a-zA-Z0-9]*`");
-            Regex final = new Regex(@"CONSTRAINT `[_a-zA-Z0-9]*`[ ]*FOREIGN KEY \(`[_a-zA-Z0-9]*`\)[ ]*REFERENCES `[_a-zA-Z0-9]*`\.`[_a-zA-Z0-9]*` \(`[_a-zA-Z0-9]*`\)([ ]*ON DELETE ((CASCADE)|(SET NULL)))?([ ]*ON UPDATE ((CASCADE)|(SET NULL)))?(,)?");
+            Regex finalForeignKey = new Regex(@"CONSTRAINT `[_a-zA-Z0-9]*`[ ]*FOREIGN KEY \(`[_a-zA-Z0-9]*`\)[ ]*REFERENCES `[_a-zA-Z0-9]*`\.`[_a-zA-Z0-9]*` \(`[_a-zA-Z0-9]*`\)([ ]*ON DELETE ((CASCADE)|(SET NULL)))?([ ]*ON UPDATE ((CASCADE)|(SET NULL)))?(,)?");
+            Regex index = new Regex(@"(UNIQUE )?INDEX `[_a-zA-Z0-9]*` \((`[_a-zA-Z0-9]*`(\([\d]*\))?( ASC| DESC)?(, )?)*\)");
 
-            foreach (var line in System.IO.File.ReadAllLines(@"<ENTER INPUT FILE PATH HERE>"))
+            const string inputFilePath = @"<ENTER INPUT FILE PATH HERE>";
+            const string dbName = "<ENTER DB NAME HERE>";
+
+            foreach (var line in System.IO.File.ReadAllLines(inputFilePath))
             {
-                if (createTable.IsMatch(line))
+                table = GetTableNameMatch(table, createTable, line);
+                if (index.IsMatch(line))
                 {
-                    table = createTable.Match(line).Groups[1].Value;
+                    output.Add($"ALTER TABLE `{dbName}`.`{table}` ADD {index.Match(line).Value};");
                 }
-                if (final.IsMatch(lineAccumulator)
-                    && final.Match(lineAccumulator + line).Length + final.Match(lineAccumulator + line).Index + 1 < lineAccumulator.Length + line.Length )
+            }
+            table = "";
+
+            output.Add("-------------------FKs from here------------------");
+
+            foreach (var line in System.IO.File.ReadAllLines(inputFilePath))
+            {
+                table = GetTableNameMatch(table, createTable, line);
+                if (finalForeignKey.IsMatch(lineAccumulator)
+                    && finalForeignKey.Match(lineAccumulator + line).Length + finalForeignKey.Match(lineAccumulator + line).Index + 1 < lineAccumulator.Length + line.Length)
                 {
                     if (lineAccumulator.EndsWith(")") || lineAccumulator.EndsWith(","))
                     {
@@ -35,7 +48,7 @@ namespace ConsoleApp1
                     {
                         lineAccumulator += ')';
                     }
-                    output.Add($"ALTER TABLE `<ENTER DBNAME HERE>`.`{table}` ADD {lineAccumulator};");
+                    output.Add($"ALTER TABLE `{dbName}`.`{table}` ADD {lineAccumulator};");
                     lineAccumulator = "";
                 }
                 if (contstraint.IsMatch(line))
@@ -49,6 +62,15 @@ namespace ConsoleApp1
             }
 
             System.IO.File.WriteAllLines(@"<ENTER OUTPUT FILE PATH HERE>", output.ToArray());
+        }
+
+        private static string GetTableNameMatch(string table, Regex createTable, string line)
+        {
+            if (createTable.IsMatch(line))
+            {
+                table = createTable.Match(line).Groups[1].Value;
+            }
+            return table;
         }
     }
 }
